b0a77f09b637e187c07fc871cda8f6e6492eb14f
hrome <58020216+hromero7@users.noreply.github.com>
2020-03-08 10:54:20 -0700

    uploading finished html page

---
+++ script.js
@@ -0,0 +1,24 @@
+// get quiz to start once start quiz button is clicked
+// display question once quiz starts
+// hide start quiz button once quiz starts
+// start timer once quiz starts
+// use local storage to save high scores
+// use objects to save question and answer data
+// clear high scores will clear data .empty function could work here
+
+
+var questionEl = $("#question");
+
+var answerBtns = $(".button");
+
+var startBtn = $("#start-btn");
+
+$(document).ready(function() {
+
+startBtn.hide(); 
+    
+
+
+})
+
+
