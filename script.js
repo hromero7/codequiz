b3f5d1e67545535f6d1f7671104380e38c4f71a8
hrome <58020216+hromero7@users.noreply.github.com>
2020-03-09 17:47:22 -0700

    removed pseudo code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// get quiz to start once start quiz button is clicked
-// display question once quiz starts
-// hide start quiz button once quiz starts
-// start timer once quiz starts
-// use local storage to save high scores
-// use objects to save question and answer data
-// clear high scores will clear data .empty function could work here
 var questionEl = $("#question");
 
 var answerBtns = $(".button");
